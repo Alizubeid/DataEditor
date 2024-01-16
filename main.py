9dc769d57b0820d25624dfc50ab370123709b6a9
while( true ){ eat; coding; sleep; } <98078407+Alizubeid@users.noreply.github.com>
2024-01-16 20:11:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from colorama import Fore as fr
 from pyfiglet import figlet_format as banner
 from os import system
 from platform import uname
+
+
 def cleanUp():
     os_name = uname().system
     if os_name == "Windows":
@@ -11,6 +13,8 @@ def cleanUp():
         system("clear")
     else:
         system("clear")
+
+
 def print_banner():
     print(fr.LIGHTCYAN_EX+banner("Team 5")+fr.LIGHTGREEN_EX)
     print("""
@@ -22,33 +26,42 @@ team 5 telegram IDs : @amirhshf021, @Pedisdi, @Zobeid_c"""+fr.LIGHTWHITE_EX)
     remove         = 3
     search         = 4
     quit           = 99
-    {+fr.LIGHTYELLOW_EX+}[NOTIF] -> this program just accept JSON files]{+fr.LIGHTWHITE_EX}
 {"#"*28}
 """
     print(text_viwe)
+
 
 def _add():
     cleanUp()
     getInput.add()
 
+
 def _edit():
     cleanUp()
     getInput.edit()
+
+
 def _remove():
     cleanUp()
     getInput.remove()
+
+
 def _search():
     cleanUp()
     getInput._search()
+
+
 def _build():
     pass
+
 
 def main():
     while True:
         cleanUp()
         print_banner()
-        user_input_opration=input(fr.LIGHTYELLOW_EX+"[*] wich opration you wanna used : "+fr.LIGHTCYAN_EX)
-        if user_input_opration in str(list(range(1,6))+[99]):
+        user_input_opration = input(
+            fr.LIGHTYELLOW_EX+"[*] wich opration you wanna used : "+fr.LIGHTCYAN_EX)
+        if user_input_opration in str(list(range(1, 6))+[99]):
             if user_input_opration == "1":
                 _add()
             elif user_input_opration == "2":
@@ -62,9 +75,8 @@ def main():
             elif user_input_opration == "99":
                 exit()
         else:
-            input(fr.LIGHTRED_EX+"[!] please enter a right number, press enter to run again..."+fr.LIGHTWHITE_EX)
-        
-
+            input(
+                fr.LIGHTRED_EX+"[!] please enter a right number, press enter to run again..."+fr.LIGHTWHITE_EX)
 
 
 if __name__ == "__main__":
