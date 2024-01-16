52d61af24b38115e29d7a67f921be577bed90b52
while( true ){ eat; coding; sleep; } <98078407+Alizubeid@users.noreply.github.com>
2024-01-16 19:32:56 +0330

    Update getInput.py

--- __get_input__/getInput.py
+++ __get_input__/getInput.py
@@ -1,4 +1,4 @@
-from __opration__ import readFile,addData,editData,deleteData,search,createFile
+from __opration__ import readFile, addData, editData, deleteData, search, createFile
 from colorama import Fore as fr
 
 
@@ -17,20 +17,25 @@ def add():
     path = input(fr.LIGHTYELLOW_EX+"Path : "+fr.LIGHTCYAN_EX)
     name = input(fr.LIGHTYELLOW_EX+"Name : "+fr.LIGHTCYAN_EX).title()
     data = readFile.readJsonFile(path)
+    print()
     if data != False:
         keys = dict(list(data.values())[0]).keys()
         add_data = addData.add_data(data, name, keys)
-        path_save = input(fr.LIGHTYELLOW_EX +
-                          "Save (path, filename): "+fr.LIGHTCYAN_EX).split()
-        try:
-            if createFile.create_file(add_data, path_save[0], path_save[1]):
-                input(
-                    fr.LIGHTGREEN_EX+"[+] seccuessfly your data has been added, press Enter to return in main page..."+fr.RESET)
-            else:
-                raise
-        except IndexError:
-            input(
-                fr.LIGHTRED_EX+"[!] your path and file name for save have problem please try again, press Enter to return main page..."+fr.RESET)
+        if add_data != False:
+            save_file = input(fr.LIGHTYELLOW_EX +
+                            "Save (path, filename): "+fr.LIGHTCYAN_EX).split()
+            try:
+                save_file[0]
+                try:
+                    save_file[1]
+                except:
+                    save_file[1] = "contant.json"
+            except IndexError:
+                save_file = ["./", "contant.json"]
+            if createFile.create_file(data, save_file[0], save_file[1]):
+                input(fr.LIGHTGREEN_EX+"[+] seccuessfly your data has been added, press Enter to return in main page..."+fr.RESET)
+        else:
+            input(fr.LIGHTGREEN_EX+f"[!] {fr.LIGHTYELLOW_EX+name+fr.LIGHTGREEN_EX} already exiested please try again, press Enter to return in main page...")
     else:
         input(fr.LIGHTRED_EX+f"[!] the {
               name} already existed please try again, press Enter to return in main page...")
@@ -52,12 +57,24 @@ def edit():
         save_file = input(fr.LIGHTYELLOW_EX +
                           "Save (path, filename) : "+fr.LIGHTCYAN_EX).split()
         try:
-            if createFile.create_file(new_data, new_data[0], save_file[1]):
-                input(
-                    fr.LIGHTGREEN_EX+f"[+] seccuessfly your edit opration, press Enter to return in main page..."+fr.RESET)
+            save_file[0]
+            try:
+                save_file[1]
+            except:
+                save_file[1] = "contant.json"
         except IndexError:
-            input(
-                fr.LIGHTRED_EX+"[!] your path and filename for save have problem please try again, press Enter to return main page..."+fr.RESET)
+            save_file = ["./", "contant.json"]
+        if createFile.create_file(data, save_file[0], save_file[1]):
+            input(fr.LIGHTGREEN_EX +
+                  f"[+] seccuessfly your edit opration, press Enter to return in main page..."+fr.RESET)
+
+        # try:
+        #     if createFile.create_file(new_data, new_data[0], save_file[1]):
+        #         input(
+        #             fr.LIGHTGREEN_EX+f"[+] seccuessfly your edit opration, press Enter to return in main page..."+fr.RESET)
+        # except IndexError:
+        #     input(
+        #         fr.LIGHTRED_EX+"[!] your path and filename for save have problem please try again, press Enter to return main page..."+fr.RESET)
     else:
         input(fr.LIGHTRED_EX +
               "[!] your Name not define in data please try again, press Enter to return in main page..."+fr.RESET)
@@ -73,20 +90,31 @@ def remove():
     path = input(fr.LIGHTYELLOW_EX+"Path : "+fr.RESET)
     name = input(fr.LIGHTYELLOW_EX+"Name : "+fr.RESET).title()
     data = readFile.readJsonFile(path)
-    data_removed = deleteData.delete_data(data,name)
+    data_removed = deleteData.delete_data(data, name)
     path_save = input(fr.LIGHTYELLOW_EX +
-                          "Save (path, filename) : "+fr.LIGHTCYAN_EX).split()
-    remove_access = input(fr.LIGHTYELLOW_EX+f"are you sure for remove user{fr.LIGHTGREEN_EX+name+fr.LIGHTYELLOW_EX} ({fr.LIGHTRED_EX+"yes"+fr.LIGHTYELLOW_EX+", "+fr.LIGHTGREEN_EX+"no"+fr.LIGHTWHITE_EX}) : "+fr.RESET)
+                      "Save (path, filename) : "+fr.LIGHTCYAN_EX).split()
+    remove_access = input(fr.LIGHTYELLOW_EX+f"are you sure for remove user{fr.LIGHTGREEN_EX+name+fr.LIGHTYELLOW_EX} ({
+                          fr.LIGHTRED_EX+"yes"+fr.LIGHTYELLOW_EX+", "+fr.LIGHTGREEN_EX+"no"+fr.LIGHTWHITE_EX}) : "+fr.RESET)
     if remove_access == "yes":
         try:
-            if createFile.create_file(data_removed,path_save[0],path_save[1]):
-                input(fr.LIGHTGREEN_EX+"[+] the has been removed")
+            save_file[0]
+            try:
+                save_file[1]
+            except IndexError:
+                save_file[1] = "contant.json"
         except IndexError:
-            input(
-                fr.LIGHTRED_EX+"[!] your path and filename for save have problem please try again, press Enter to return main page..."+fr.RESET)
+            save_file = ["./", "contant"]
+        if createFile.create_file(data_removed, path_save[0], path_save[1]):
+            input(fr.LIGHTGREEN_EX+"[+] the has been removed")
+        # try:
+        #     if createFile.create_file(data_removed, path_save[0], path_save[1]):
+        #         input(fr.LIGHTGREEN_EX+"[+] the has been removed")
+        # except IndexError:
+        #     input(
+        #         fr.LIGHTRED_EX+"[!] your path and filename for save have problem please try again, press Enter to return main page..."+fr.RESET)
     else:
-        input(fr.LIGHTWHITE_EX+"[!] data not removed, press Enter to return in main page..."+fr.RESET)
-
+        input(fr.LIGHTWHITE_EX +
+              "[!] data not removed, press Enter to return in main page..."+fr.RESET)
 
 
 def _search():
@@ -99,10 +127,9 @@ def _search():
     path = input(fr.LIGHTYELLOW_EX+"Path : "+fr.RESET)
     name = input(fr.LIGHTYELLOW_EX+"Name : "+fr.RESET).title()
     data = readFile.readJsonFile(path)
-    search_data = search.search_data(data,name)
+    search_data = search.search_data(data, name)
     if search_data:
         input(fr.LIGHTWHITE_EX+"Press Enter to return in main page..."+fr.RESET)
-
 
 
 def build():
